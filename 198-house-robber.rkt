e973867ff8a13b69d6defd8510b18c985c4e0cb9
s-cerevisiae <spore2050@gmail.com>
2021-06-02 14:58:56 +0800

    Add solution for #198 and split up

--- 198-house-robber.rkt
+++ 198-house-robber.rkt
@@ -1,17 +1,13 @@
 #lang racket
 
-(module solution lazy
-  (provide rob)
-
-  (define (rob nums)
-    (define len (length nums))
-    (define dp
-      (for/vector ([i (in-naturals)]
-                   [x (in-list nums)])
-        (cond [(= i 0) x]
-              [(= i 1) (max (vector-ref dp 0) x)]
-              [else (max (+ (vector-ref dp (- i 2)) x)
-                         (vector-ref dp (- i 1)))])))
-    (! (vector-ref dp (- len 1)))))
-
-(require 'solution)
+(define (rob nums)
+  (for/fold ([p1 0] [p2 0]
+             #:result p1)
+            ([i (in-naturals)]
+             [x (in-list nums)])
+    (values
+      (match i
+        [0 x]
+        [1 (max x p1)]
+        [_ (max p1 (+ x p2))])
+      p1)))
