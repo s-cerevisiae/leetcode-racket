f91e55c2aeb8cda8e4fcbcdf5ae8b2f2402d3b57
s-cerevisiae <spore2050@gmail.com>
2021-06-02 12:37:00 +0800

    Change to #lang racket

--- 1-two-sum.rkt
+++ 1-two-sum.rkt
@@ -1,21 +1,19 @@
-#lang typed/racket
+#lang racket
 
-(: two-sum ([Listof Integer] Integer -> [Listof Integer]))
 (define (two-sum nums target)
-  (define table (#{make-hash @ Integer Integer}))
-  (or (for/last : (U False (List Integer Integer))
-                 ([i (in-naturals)]
-                  [x (in-list nums)]
-                  #:final (hash-has-key? table (- target x)))
-        (hash-set! table x i)
-        (list (hash-ref table (- target x)) i))
-      (list)))
+  (define table (make-hash))
+  (for/last ([i (in-naturals)]
+             [x (in-list nums)]
+             #:final (hash-has-key? table (- target x)))
+    (hash-set! table x i)
+    (list (hash-ref table (- target x)) i)))
 
-(: two-sum-func ([Listof Integer] Integer -> [Listof Integer]))
 (define (two-sum-func nums target)
-  (let loop ([table (#{hash @ Integer Integer})] [i 0] [l nums])
-    (match-define (cons x xs) l)
-    (define maybe-pos (hash-ref table (- target x) #f))
-    (if maybe-pos
-        (list maybe-pos i)
-        (loop (hash-set table x i) (add1 i) xs))))
+  (let loop ([table (hash)] [i 0] [l nums])
+    (match l
+      ['() #f]
+      [(cons x xs)
+       (define maybe-pos (hash-ref table (- target x) #f))
+       (if maybe-pos
+           (list maybe-pos i)
+           (loop (hash-set table x i) (add1 i) xs))])))
