eacd5f4617386fea0534fdc9af89946d92969264
s-cerevisiae <spore2050@gmail.com>
2021-06-23 21:52:46 +0800

    Add tree-node.rkt

---
+++ tree-node.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(provide (struct-out tree-node)
+         make-tree-node)
+
+; val : integer?
+; left : (or/c tree-node? #f)
+; right : (or/c tree-node? #f)
+(struct tree-node
+  (val left right) #:mutable #:transparent)
+
+; constructor
+(define (make-tree-node [val 0])
+  (tree-node val #f #f))
