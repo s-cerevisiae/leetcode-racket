5327da740de69b9af7cef08572d1c750f05cc95d
s-cerevisiae <spore2050@gmail.com>
2022-03-15 12:18:19 +0800

    Add list->tree and tree->list to tree-node.rkt

--- tree-node.rkt
+++ tree-node.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (provide (struct-out tree-node)
-         make-tree-node)
+         make-tree-node
+         tree->list
+         list->tree)
 
 ; val : integer?
 ; left : (or/c tree-node? #f)
@@ -12,3 +14,16 @@
 ; constructor
 (define (make-tree-node [val 0])
   (tree-node val #f #f))
+
+(define/match (tree->list node)
+  [(#f) '()]
+  [((tree-node val #f #f))
+   val]
+  [((tree-node val left right))
+   (list val (tree->list left) (tree->list right))])
+
+(define/match (list->tree node)
+  [('()) #f]
+  [((list val left right))
+   (tree-node val (list->tree left) (list->tree right))]
+  [(val) (make-tree-node val)])
